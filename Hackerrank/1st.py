ca5ab532ec52e6097195f4eb8c70681da4c323f7
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-05-12 16:41:09 +0530

    Hackerrank

---
+++ Hackerrank/1st.py
@@ -0,0 +1,41 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+
+def dayOfProgrammer(year):
+    # Write your code here
+    if(year == 1918):
+        return "26.09.1918"
+    elif (year <= 1917):
+        if (year % 4 == 0):
+            return "12.09.{}".format(year)
+        else:
+            return "13.09.{}".format(year)
+    else:
+        if((year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)):
+            return "12.09.{}".format(year)
+        else:
+            return "13.09.{}".format(year)
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+    print(result)
+    # fptr.write(result + '\n')
+
+    # fptr.close()
---
+++ Hackerrank/3rd.py
@@ -0,0 +1,40 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+
+def pickingNumbers(a):
+    # Write your code here
+    count = {}
+    for i in a:
+        count[i] = count.get(i, 0) + 1
+   
+    print(count)
+    max = 0
+    for i in count:
+        if count[i] >
+    return 0
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
