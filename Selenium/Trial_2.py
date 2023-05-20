0d87b7bc03d58999101ed9c7ee644a147d105fba
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-05-20 12:45:18 +0530

    yay it worked

--- Selenium/Trial_2.py
+++ Selenium/Trial_2.py
@@ -1,25 +1,12 @@
-import time
+# Python program to demonstrate
+# selenium
+
+# import webdriver
 from selenium import webdriver
 
-PATH = "/Users/apple/Downloads/chromedriver_mac_arm64/chromedriver"
-driver = webdriver.Chrome(PATH)
+# create webdriver object
 
-# Instruct the WebDriver object to open the URL of your localhost application
-driver.get("http://www.google.com")
+driver = webdriver.Chrome("/Users/apple/Downloads/chromedriver_mac_arm64/chromedriver")
 
-# Enter your username and password in the login form
-username_input = driver.find_element_by_id("Gmail")
-username_input.send_keys("your_username")
-
-password_input = driver.find_element_by_id("Images")
-password_input.send_keys("your_password")
-
-# Click on the login button
-login_button = driver.find_element_by_id("login_button")
-login_button.click()
-
-# Wait for the login to be successful
-time.sleep(5)
-
-# Verify that you are logged in by checking the page title
-assert driver.title == "Your localhost application"
+# get google.co.in
+driver.get("https://google.com/search?q=geeksforgeeks")
