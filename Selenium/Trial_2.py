b86d7584f83141ffa1221fe338fb9fc881b1bf8e
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-05-20 19:05:56 +0530

    final one

--- Selenium/Trial_2.py
+++ Selenium/Trial_2.py
@@ -1,12 +1,24 @@
-# Python program to demonstrate
-# selenium
-
 # import webdriver
 from selenium import webdriver
 
-# create webdriver object
+# import Action chains
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
+# create webdriver object
 driver = webdriver.Chrome("/Users/apple/Downloads/chromedriver_mac_arm64/chromedriver")
 
-# get google.co.in
-driver.get("https://google.com/search?q=geeksforgeeks")
+driver.maximize_window() # For maximizing window
+
+# get geeksforgeeks.org
+driver.get("https://www.geeksforgeeks.org/")
+
+driver.maximize_window() # For maximizing window
+
+driver.implicitly_wait(5) 
+
+search = driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div[1]/div[1]/div[2]/span/span/span[1]/input")
+search.send_keys("Python")
+search.send_keys(Keys.RETURN)
+
+
