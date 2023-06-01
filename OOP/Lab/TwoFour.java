fb1a9f64ab3290346483a0cd6997ed2d47e3b326
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-06-01 14:19:33 +0530

    small change to make it user input suitables

--- OOP/Lab/TwoFour.java
+++ OOP/Lab/TwoFour.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class TwoFour {
     public static void main(String[] args) throws CloneNotSupportedException {
         MyPriorityQueue<Integer> q1 = new MyPriorityQueue<>();
-        q1.offer(110);
-        q1.offer(20);
-        q1.offer(50);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("How many numbers do you want to input: ");
+            int n = sc.nextInt();
+            System.out.println("\n");
+            while (n > 0) {
+                System.out.println("Enter a number: ");
+                int a = sc.nextInt();
+                q1.offer(a);
+                n--;
+            }
+        }
         MyPriorityQueue<Integer> q2 = q1.clone();
-        System.out.println("Queue1: ");
+        System.out.println("\n\nQueue1: ");
         while (q1.size() > 0) {
             System.out.print(q1.poll() + " ");
         }
