76999c3bb558157286f472046953ee8f889fac17
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-08-03 19:09:05 +0530

    Changes in the way

--- ComputerVision/ComputerVision.py
+++ ComputerVision/ComputerVision.py
@@ -4,7 +4,7 @@ from ImageVision import detect_red_box
 
 def extract_text_from_video(video_path, output_file):
     video_capture = cv2.VideoCapture(video_path)
-
+    stringResult = " "
     with open(output_file, 'w') as text_file:
         while True:
             ret, frame = video_capture.read()
@@ -18,11 +18,13 @@ def extract_text_from_video(video_path, output_file):
             # Write the extracted text to the output file
             listToStr = ' '.join([str(elem) for elem in text])
             text_file.write(listToStr + "\n")
-
+            stringResult += listToStr + "\n"
     video_capture.release()
+    return stringResult
 
 if __name__ == "__main__":
     video_file_path = "/Users/apple/Downloads/ocr/5.mp4"
     output_text_file = "text.txt"
 
-    extract_text_from_video(video_file_path, output_text_file)
+    text = extract_text_from_video(video_file_path, output_text_file)
+    print(text)
