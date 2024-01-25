9de0970eccdfa38b800230902d49e3a90a25fec7
Dv04 <62090715+Dv04@users.noreply.github.com>
2024-01-25 12:21:08 +0530

    Refactor encryption and decryption functions

--- College/Data_Security/transposition.py
+++ College/Data_Security/transposition.py
@@ -39,7 +39,7 @@ def rail_fence_decrypt(cipher, key):
     count = 0
     for i in range(key):
         step = key * 2 - 2
-        for j in range(i, length, key):
+        for j in range(i, length, step):
             plain[j] = cipher[count]
             count += 1
             if i != 0 and i != key - 1 and j + step - 2 * i < length:
@@ -49,16 +49,49 @@ def rail_fence_decrypt(cipher, key):
 
 
 text = "Hello everyone this is Dev"
-key = 3
+key = int(input("Enter the key: "))
 
 swap_encrypted = swap_encrypt(text)
-print("Swap Encrypted: ", swap_encrypted)
+print("Swap Encrypted: ", swap_encrypted, "\n")
 
 swap_decrypted = swap_decrypt(swap_encrypted)
-print("Swap Decrypted: ", swap_decrypted)
+print("Swap Decrypted: ", swap_decrypted, "\n")
 
 encrypted = rail_fence_encrypt(text, key)
-print("Encrypted: ", encrypted)
+print("Encrypted: ", encrypted, "\n")
 
 decrypted = rail_fence_decrypt(encrypted, key)
-print("Decrypted: ", decrypted)
+print("Decrypted: ", decrypted, "\n")
+
+
+def reverse_swap_encrypt(text):
+    text = text[::-1]  # Reverse the text
+    return swap_encrypt(text)
+
+
+def reverse_swap_decrypt(text):
+    text = swap_decrypt(text)
+    return text[::-1]  # Reverse the text back
+
+
+def reverse_rail_fence_encrypt(text, key):
+    text = text[::-1]  # Reverse the text
+    return rail_fence_encrypt(text, key)
+
+
+def reverse_rail_fence_decrypt(cipher, key):
+    text = rail_fence_decrypt(cipher, key)
+    return text[::-1]  # Reverse the text back
+
+
+reverse_swap_encrypted = reverse_swap_encrypt(text)
+print("Reverse Swap Encrypted: ", reverse_swap_encrypted, "\n")
+
+reverse_swap_decrypted = reverse_swap_decrypt(reverse_swap_encrypted)
+print("Reverse Swap Decrypted: ", reverse_swap_decrypted, "\n")
+
+reverse_encrypted = reverse_rail_fence_encrypt(text, key)
+print("Reverse Encrypted: ", reverse_encrypted, "\n")
+
+reverse_decrypted = reverse_rail_fence_decrypt(reverse_encrypted, key)
+print("Reverse Decrypted: ", reverse_decrypted, "\n")
