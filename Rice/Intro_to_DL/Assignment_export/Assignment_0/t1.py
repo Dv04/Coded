2677d056f441295f5e807522e5772c4a4aec4cd1
Dv04 <62090715+Dv04@users.noreply.github.com>
2025-09-14 16:23:01 -0500

    Changes

--- Rice/Intro_to_DL/Assignment_export/Assignment_0/t1.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3, 4], [1, 2, 7, 14])
-plt.axis([0, 6, 0, 20])
-plt.show()
---
+++ Rice/Intro_to_DL/Assignment_export/Assignment_0/t3.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 2, 7, 14])
+plt.axis([0, 6, 0, 20])
+plt.show()
